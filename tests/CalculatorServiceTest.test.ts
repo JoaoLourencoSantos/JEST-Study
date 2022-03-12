23538fe97f6f96dbacbaffe6b489038aabc0637f
Lais Helena <lah-paula@live.com>
2022-03-12 15:37:09 -0300

    adcionando casos de testes

--- tests/CalculatorServiceTest.test.ts
+++ tests/CalculatorServiceTest.test.ts
@@ -13,6 +13,17 @@ describe(`${title} - Addition`, () => {
     expect(CalculatorService.sum(-5, 2)).toBe(-3);
     expect(CalculatorService.sum(0, 2)).toBe(2);
   });
+
+  it(`${valid} - double case`, () => {
+    expect(CalculatorService.sum(0.5, 2)).toBe(2.5);
+    expect(CalculatorService.sum(1.3, 2)).toBe(3.3);
+    expect(CalculatorService.sum(-0.5, 2)).toBe(1.5);
+  });
+  it(`${error} - value`, () => {
+    expect(() => {
+      CalculatorService.sum(5, "s");
+    }).toThrow(InvalidNumberException);
+  });
 });
 
 describe(`${title} - Subtraction`, () => {
@@ -20,6 +31,18 @@ describe(`${title} - Subtraction`, () => {
     expect(CalculatorService.sub(5, 2)).toBe(3);
     expect(CalculatorService.sub(5, -2)).toBe(7);
     expect(CalculatorService.sub(10, 10)).toBe(0);
+  });
+
+  it(`${valid} - double case`, () => {
+    expect(CalculatorService.sub(0.5, 2)).toBe(-1.5);
+    expect(CalculatorService.sub(1.3, 2)).toBe(-0.7);
+    expect(CalculatorService.sub(-0.5, 2)).toBe(-2.5);
+  });
+
+  it(`${error} - value`, () => {
+    expect(() => {
+      CalculatorService.sub(5, "s");
+    }).toThrow(InvalidNumberException);
   });
 });
 
@@ -29,6 +52,18 @@ describe(`${title} - Multiplication`, () => {
     expect(CalculatorService.mult(-5, 2)).toBe(-10);
     expect(CalculatorService.mult(1, 0)).toBe(0);
   });
+
+  it(`${valid} - double case`, () => {
+    expect(CalculatorService.mult(0.5, 2)).toBe(1);
+    expect(CalculatorService.mult(1.3, 2)).toBe(2.6);
+    expect(CalculatorService.mult(-0.5, 2)).toBe(-1);
+  });
+
+  it(`${error} - value`, () => {
+    expect(() => {
+      CalculatorService.sub(5, "s");
+    }).toThrow(InvalidNumberException);
+  });
 });
 
 describe(`${title} - Division`, () => {
@@ -36,7 +71,15 @@ describe(`${title} - Division`, () => {
     expect(CalculatorService.div(5, 2)).toBe(2.5);
     expect(CalculatorService.div(-5, 2)).toBe(-2.5);
     expect(CalculatorService.div(1, 2)).toBe(0.5);
+    expect(CalculatorService.div(0, 2)).toBe(0);
   });
+
+  it(`${valid} - double case`, () => {
+    expect(CalculatorService.div(2.5, 1.6)).toBe(1.5625);
+    expect(CalculatorService.mult(1.3, 2)).toBe(0.65);
+    expect(CalculatorService.mult(-0.5, 2)).toBe(-0.25);
+  });
+
   it(`${error} - value`, () => {
     expect(() => {
       CalculatorService.div(5, "s");
